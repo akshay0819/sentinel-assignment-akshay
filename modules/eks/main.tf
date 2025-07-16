de6c368edb5958acec5197ac34d92c5fde9b4a95
akshay0819 <nambiarakshay84@gmail.com>
2025-07-16 13:36:54 +0400

    eks gateway and eks backend modules

---
+++ modules/eks/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "eks.amazonaws.com" }
+      Effect    = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = var.name == "eks-gateway" ? true : false # only gateway public
+  }
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "node_group_role" {
+  name = "${var.name}-nodegroup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Effect    = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ])
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = each.value
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.name}-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = var.private_subnet_ids
+  instance_types  = var.instance_types
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_size
+    min_size     = var.min_size
+  }
+
+  tags = var.tags
+}
---
+++ modules/eks/outputs.tf
@@ -0,0 +1,50 @@
+variable "name" {
+  type        = string
+  description = "EKS cluster name"
+}
+
+variable "cluster_version" {
+  type        = string
+  default     = "1.29"
+  description = "EKS Kubernetes version"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for the EKS cluster"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private subnet IDs for EKS"
+}
+
+variable "instance_types" {
+  type        = list(string)
+  default     = ["t3.medium"]
+  description = "Instance types for EKS nodes"
+}
+
+variable "desired_capacity" {
+  type        = number
+  default     = 2
+}
+
+variable "min_size" {
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  type        = number
+  default     = 3
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
+
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
