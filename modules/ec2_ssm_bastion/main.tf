ecc2b7fd81fe25eb90f80bd48fa9d5042579bba5
akshay0819 <nambiarakshay84@gmail.com>
2025-07-17 09:14:55 +0400

    send command using instance name

--- modules/ec2_ssm_bastion/main.tf
+++ modules/ec2_ssm_bastion/main.tf
@@ -77,6 +77,14 @@ resource "aws_security_group" "bastion" {
   description = "Allow internal access to VPC endpoints and EKS"
   vpc_id      = var.vpc_id
 
+  ingress {
+    description = "Allow SSM from anywhere"           
+    from_port   = 443                                 
+    to_port     = 443                                 
+    protocol    = "tcp"                              
+    cidr_blocks = ["0.0.0.0/0"]                       
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
